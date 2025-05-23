db7f024944bad56e5841c5c76ef1cf1340a2bb42
Aldf76 <contasfelipe623@gmail.com>
2025-05-23 13:32:12 -0300

    fix: corrigir baseURL do axios e ajustar serviços para lidar com resposta paginada

---
+++ src/services/unidadeService.ts
@@ -0,0 +1,23 @@
+// src/services/unidadeService.ts
+import { api } from "@/lib/axios";
+import { Unidade, Pagina } from "@/types/types";
+
+// Tipo auxiliar para cadastro
+type NovaUnidade = Omit<Unidade, "id" | "ativo">;
+
+// POST /unidades
+export const cadastrarUnidade = async (
+  dados: NovaUnidade
+): Promise<Unidade> => {
+  const response = await api.post<Unidade>("/unidades", dados);
+  return response.data;
+};
+
+// GET /unidades
+export const listarUnidades = async (): Promise<Unidade[]> => {
+    const response = await api.get<Pagina<Unidade>>("/unidades");
+    return response.data.content;
+  };
+  
+
+
