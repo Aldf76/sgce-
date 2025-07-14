64828c4fbc9684b044ce064870ad2ae1c8ba4712
Aldf76 <contasfelipe623@gmail.com>
2025-07-14 20:50:42 -0300

    Facilitando o acesso à rota page Home

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,11 +1,15 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 export function Header() {
   return (
     <header className="bg-[#1E2547] text-white py-4 px-6 shadow-md flex items-center gap-4">
+    {/* Título clicável que redireciona para a página inicial */}
+    <Link to="/" className="hover:opacity-90 transition-opacity">
       <h1 className="text-xl md:text-2xl font-semibold tracking-wide">
         Sistema de Gestão de Consumo Energético
       </h1>
-    </header>
+    </Link>
+  </header>
   );
 }
